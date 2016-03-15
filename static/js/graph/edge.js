cd4bdfbd74039583c1d2f09ab33856c0d7c3646a
Oliver Bestmann <obest@esailors.de>
2016-03-15 09:00:43 +0000

    Introduce private namespaces for classes.

--- static/js/graph/edge.js
+++ static/js/graph/edge.js
@@ -1,54 +1,57 @@
+const GraphEdgeView = (() => {
+  class GraphEdgeView extends View {
+    /**
+     * Connects two positions
+     *
+     * @param {Rx.Observable} source
+     * @param {Rx.Observable} target
+     */
+    init(source, target) {
+      this._source = source.map(Vector.of);
+      this._target = target.map(Vector.of);
+    }
 
-class GraphEdgeView extends View {
-  /**
-   * Connects two positions
-   *
-   * @param {Rx.Observable} source
-   * @param {Rx.Observable} target
-   */
-  init(source, target) {
-    this._source = source.map(Vector.of);
-    this._target = target.map(Vector.of);
+    render() {
+      const view = createElement("div", "graph__edge");
+
+      const closer = Rx.Observable.merge(
+        this._source.ignoreElements().concat(Rx.Observable.just(true)),
+        this._target.ignoreElements().concat(Rx.Observable.just(true)));
+
+      Rx.Observable.combineLatest(this._source, this._target)
+        .takeUntil(closer)
+
+        .finally(() => this.destroy())
+        .subscribe(args => {
+          const [source, target] = args;
+          const angle = source.angleTo(target);
+          const length = source.distanceTo(target);
+
+          const st = view.style;
+          st.top = `${source.y}px`;
+          st.left = `${source.x}px`;
+          st.width = `${length}px`;
+          st.transform = `rotate(${angle}rad)`;
+        });
+
+      return view;
+    }
+
+    ping(reversed = false, duration = 2000) {
+      const $packet = createElement("div",
+        "graph__edge__packet",
+        `graph__edge__packet--${reversed ? 'reverse' : 'normal'}`);
+
+      $packet.style.transitionDuration = `${duration}ms`;
+      this.$root.appendChild($packet);
+
+      // set the class after layout and rendering
+      Rx.Observable.timer(100)
+        .doOnNext(() => $packet.classList.add("graph__edge__packet--on"))
+        .delay(100 + duration)
+        .subscribe(() => $packet.parentNode.removeChild($packet));
+    }
   }
 
-  render() {
-    const view = createElement("div", "graph__edge");
-
-    const closer = Rx.Observable.merge(
-      this._source.ignoreElements().concat(Rx.Observable.just(true)),
-      this._target.ignoreElements().concat(Rx.Observable.just(true)));
-
-    Rx.Observable.combineLatest(this._source, this._target)
-      .takeUntil(closer)
-
-      .finally(() => this.destroy())
-      .subscribe(args => {
-        const [source, target] = args;
-        const angle = source.angleTo(target);
-        const length = source.distanceTo(target);
-
-        const st = view.style;
-        st.top = `${source.y}px`;
-        st.left = `${source.x}px`;
-        st.width = `${length}px`;
-        st.transform = `rotate(${angle}rad)`;
-      });
-
-    return view;
-  }
-
-  ping(reversed = false, duration = 2000) {
-    const $packet = createElement("div",
-      "graph__edge__packet",
-      `graph__edge__packet--${reversed ? 'reverse' : 'normal'}`);
-
-    $packet.style.transitionDuration = `${duration}ms`;
-    this.$root.appendChild($packet);
-
-    // set the class after layout and rendering
-    Rx.Observable.timer(100)
-      .doOnNext(() => $packet.classList.add("graph__edge__packet--on"))
-      .delay(100 + duration)
-      .subscribe(() => $packet.parentNode.removeChild($packet));
-  }
-}
+  return GraphEdgeView;
+})();
--- static/js/graph/node.js
+++ static/js/graph/node.js
@@ -1,95 +1,98 @@
+const GraphNodeView = (() => {
+  class GraphNodeView extends View {
+    init(id) {
+      this._id = id;
+      this._position = new Rx.BehaviorSubject();
+      this._size = undefined;
+    }
 
-class GraphNodeView extends View {
-  init(id) {
-    this._id = id;
-    this._position = new Rx.BehaviorSubject();
-    this._size = undefined;
-  }
+    render() {
+      const el = createElement("div", "graph__node");
+      el.innerText = this.id;
+      return el;
+    }
 
-  render() {
-    const el = createElement("div", "graph__node");
-    el.innerText = this.id;
-    return el;
-  }
+    destroy() {
+      super.destroy();
+      this._position.onCompleted();
+    }
 
-  destroy() {
-    super.destroy();
-    this._position.onCompleted();
-  }
+    /**
+     * The id of this node.
+     * @returns {String}
+     */
+    get id() {
+      return this._id;
+    }
 
-  /**
-   * The id of this node.
-   * @returns {String}
-   */
-  get id() {
-    return this._id;
-  }
+    /**
+     * Returns an observable with the current position of this node.
+     * @returns {Rx.Observable<Vector>}
+     */
+    get rxPosition() {
+      return this._position.filter(pos => pos != null).map(Vector.of);
+    }
 
-  /**
-   * Returns an observable with the current position of this node.
-   * @returns {Rx.Observable<Vector>}
-   */
-  get rxPosition() {
-    return this._position.filter(pos => pos != null).map(Vector.of);
-  }
+    /**
+     * Get the radius by using half of the maximum of height and width.
+     * @returns {number}
+     */
+    get radius() {
+      return Math.max(this.width, this.height) / 2;
+    }
 
-  /**
-   * Get the radius by using half of the maximum of height and width.
-   * @returns {number}
-   */
-  get radius() {
-    return Math.max(this.width, this.height) / 2;
-  }
+    /**
+     * Returns the position of this node.
+     * @returns {Vector}
+     */
+    get position() {
+      return super.position.plus(this.size.scaled(0.5));
+    }
 
-  /**
-   * Returns the position of this node.
-   * @returns {Vector}
-   */
-  get position() {
-    return super.position.plus(this.size.scaled(0.5));
-  }
+    get width() {
+      return this.size.x;
+    }
 
-  get width() {
-    return this.size.x;
-  }
+    get height() {
+      return this.size.y;
+    }
 
-  get height() {
-    return this.size.y;
-  }
+    get size() {
+      return this._size
+        ? this._size
+        : this._size = super.size;
+    }
 
-  get size() {
-    return this._size
-      ? this._size
-      : this._size = super.size;
-  }
+    get alias() {
+      return this.$root.innerText;
+    }
 
-  get alias() {
-    return this.$root.innerText;
-  }
+    set alias(alias) {
+      this.$root.innerText = alias;
+    }
 
-  set alias(alias) {
-    this.$root.innerText = alias;
-  }
+    get selected() {
+      return this.$root.classList.contains("graph__node--selected");
+    }
 
-  get selected() {
-    return this.$root.classList.contains("graph__node--selected");
-  }
+    set selected(selected) {
+      if (selected) {
+        this.$root.classList.add("graph__node--selected");
+      } else {
+        this.$root.classList.remove("graph__node--selected");
+      }
+    }
 
-  set selected(selected) {
-    if (selected) {
-      this.$root.classList.add("graph__node--selected");
-    } else {
-      this.$root.classList.remove("graph__node--selected");
+    /**
+     * Changes the position of this node. This will trigger a change in
+     * the position observable.
+     */
+    moveTo(pos) {
+      const center = Vector.of(pos);
+      super.moveTo(center.minus(this.size.scaled(0.5)));
+      this._position.onNext(center);
     }
   }
 
-  /**
-   * Changes the position of this node. This will trigger a change in
-   * the position observable.
-   */
-  moveTo(pos) {
-    const center = Vector.of(pos);
-    super.moveTo(center.minus(this.size.scaled(0.5)));
-    this._position.onNext(center);
-  }
-}
+  return GraphNodeView;
+})();
